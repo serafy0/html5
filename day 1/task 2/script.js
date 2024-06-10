ecbd72ace034d1b09953ec44a8770e4b548ffeaa
Mohamed El-Serafy <elserafy02@gmail.com>
2024-06-10 12:43:28 +0300

    first commit

---
+++ day 1/task 2/script.js
@@ -0,0 +1,18 @@
+let inputs = document.getElementsByTagName("input")
+let text = document.getElementById("text")
+for(let i = 0;i<=2;i++){
+    inputs[i].addEventListener("change",function(){
+        changeColor()
+
+    })
+
+}
+function changeColor(){
+   let red = inputs[0].value;
+    let green = inputs[1].value;
+    let blue = inputs[2].value;
+    text.style.color = `rgb(${red},${green},${blue})`
+    console.log(text.style.color)
+}
+
+changeColor()
